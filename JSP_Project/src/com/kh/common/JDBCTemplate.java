b967bdf60c3d70f59d7b09167f1ff569a4e2b05f
yoonmyungseok <yoonms5319@gmail.com>
2022-05-03 18:22:12 +0900

    0503

---
+++ JSP_Project/src/com/kh/common/JDBCTemplate.java
@@ -0,0 +1,16 @@
+package com.kh.common;
+
+public class JDBCTemplate {
+    //1.Connection 객체 생성(DB 접속)한 후 해당 Connection 객체를 반환하는 메소드
+
+    //2.전달받은 Connection 객체를 가지고 commit 해주는 메소드
+
+    //3.전달받은 Connection 객체를 가지고 rollback 해주는 메소드
+
+    //4.전달받은 Connection 객체를 반납해주는 메소드
+
+    //5.전달받은 Statement 객체를 반납해주는 메소드
+    //=>다형성에 의해 자식클래스인 PreparedStatement 도 반납 가능
+
+    //6.전달받은 ResultSet 객체를 반납시켜주는 메소드
+}
